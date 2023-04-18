c27a69d05da2d209fd8749c3358195af6404a144
0u05630 <129100839+0u05630@users.noreply.github.com>
2023-04-19 03:30:47 +0800

    Add files via upload

--- matrix cal.py
+++ matrix cal.py
@@ -1,3 +1,40 @@
+def matrix_init(choose, row, col):
+    if choose == 'matrixA':
+        A = [[0 for x in range(col)]for y in range(row)]
+        return A
+    if choose == 'matrixB':
+        B = [[0 for x in range(col)]for y in range(row)]
+        return B
+    if choose == 'matrixC':
+        C = [[0 for x in range(col)]for y in range(row)]
+        return C
+    if choose == 'matrixD':
+        D = [[0 for x in range(col)]for y in range(row)]
+        return D
+    if choose == 'matrixE':
+        E = [[0 for x in range(col)]for y in range(row)]
+        return E
+
+
+def assign_matrix():
+    check = 0
+    choose = input('Choose the slot you want to assign (matrixA to matrixE):')
+    while choose not in ['matrixA', 'matrixB', 'matrixC', 'matrixD', 'matrixE']:
+        choose = input(
+            'There is no such slot, please choose again (matrixA to matrixE):')
+    inp = input(
+        'declare the dimension of the matrix (in term of row*column):').split('*')
+    while check < 2:
+        for x in inp:
+            if x.isdigit():
+                check = check + 1
+        if check != 2:
+            inp = input('incorrect format, plz input again:').split('*')
+    row = int(inp[0])
+    col = int(inp[1])
+    matrix_init(choose, row, col)
+
+
 def find_trace(matrix):
     if len(matrix) != len(matrix[1]):
         print('Math error')
@@ -21,13 +58,10 @@ def multiply(matrixA, matrixB):
             for j in range(len(matrixB[i])):
                 temp1 = 0
                 for a in (range(len(matrixA[1]))):
-
                     temp = 0
                     temp = matrixA[i][a] * matrixB[a][j]
                     temp1 = temp1 + temp
-
                 matrixC[i][j] = temp1
-
         print('The product of ', matrixA, 'and ', matrixB, ' is ', matrixC)
 
 
@@ -35,7 +69,6 @@ def transpose(matrix):
     temp = 0
     k = 0
     for i in range(len(matrix)):
-
         for j in range(k, len(matrix[i])):
             temp = 0
             temp = matrix[i][j]
@@ -47,15 +80,37 @@ def transpose(matrix):
 # def reducerow(matrix):
 
 
+def det(matrix):
+    det_num = 0
+    if len(matrix) == 2:
+        output = matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
+        return output
+    else:
+        for x in range(len(matrix)):
+            temp_matrix = [sub[:] for sub in matrix]
+            temp_matrix.remove(temp_matrix[x])
+            for z in temp_matrix:
+                z.pop(0)
+            if x % 2 == 0:
+                det_num = det_num + matrix[x][0]*det(temp_matrix)
+            if x % 2 != 0:
+                det_num = det_num - matrix[x][0]*det(temp_matrix)
+        return det_num
+
 
 if __name__ == '__main__':
-    A = [[1, 4, 5], [5, 4, 2], [3, 8, 12]]
-    B = [[2, 4], [8, 3], [4, 8]]
-    test = 8
-    for x in B:
-        if test in x:
-            print(B.index(x),x.index(test))
-    
+    A = [[0, 1, 4, 2, 3], [4, 2, 0, 1, 5], [
+        2, 2, 1, 0, 3], [3, 4, 5, 3, 0], [9, 5, 4, 3, 7]]
+    # assign_matrix()
+    # print('/n'.join([' '.join([''.format(item)for item in row])
+    #       for row in list(matrix_init)]))
+    # print(matrix_init)
+    print(transpose(A))
+
+    # test = 8
+    # for x in B:
+    #     if test in x:
+    #         print(B.index(x),x.index(test))
 
     # multiply(A, B)
     # find_trace(B)
